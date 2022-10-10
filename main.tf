e7135fc39d68c3c084d478f121af65bf2e0f92e3
Tyree Pearson <tyree@hashicorp.com>
2022-10-10 11:40:46 -0500

    eks getting started initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud{
+    organization = "tyreepearson"
+    workspaces{
+        names="eks-getting-started"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
